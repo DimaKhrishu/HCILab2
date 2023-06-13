58a44245f08f634b6744f7f44b322ccf1c796cd5
DimaKhrishu <dima.khrishu@gmail.com>
2023-06-13 17:03:02 +0300

    Add project files.

---
+++ HCILab2/Pages/Index.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HCILab2.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
